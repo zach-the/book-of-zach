f70c65297793c4a471725cb56c21d89816a9a588
zach-the <zachzbennett@gmail.com>
2026-01-22 06:43:05 -0700

    big bang

---
+++ make_booklet.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+if [[ -z $1 ]]; then
+    echo "usage: ./make_booklet.sh <inputfile.tex>"
+    exit
+fi
+
+
+
+latexmk -lualatex $1
+tex_basename=$(basename $1)
+pdf_basename="${tex_basename/tex/pdf}"
+latexmk -lualatex -usepretex="\def\filename{$pdf_basename}" bookletize.tex
+latexmk -c
+rm $tex_basename*
+
+echo $tex_basename
+echo $pdf_basename
+echo "now you can run:"
+echo
+echo "    open bookletize.pdf"
+echo
+echo "to view the booklet"
+
